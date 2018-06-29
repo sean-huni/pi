99f5c7262881ff8c734e3b47c86daeb952d4c473
Kudzai <khuni@gitlab.africanbank.net>
2018-06-29 17:21:15 +0200

    Completed the Remember-Me feature.

---
+++ src/main/java/io/home/pi/component/AuthenticationFailureListener.java
@@ -0,0 +1,31 @@
+package io.home.pi.component;
+
+import io.home.pi.service.impl.LoginAttemptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.stereotype.Component;
+
+/**
+ * PACKAGE : io.home.pi.component
+ * USER    : Kudzai Sean Huni
+ * TIME    : 15:40
+ * DATE    : Friday-29-June-2018
+ * E-MAIL  : kudzai@tangentsolutions.co.za
+ * CELL    : +27-78-683-1982
+ */
+@Component
+public class AuthenticationFailureListener
+        implements ApplicationListener<AuthenticationFailureBadCredentialsEvent> {
+
+    @Autowired
+    private LoginAttemptService loginAttemptService;
+
+    public void onApplicationEvent(AuthenticationFailureBadCredentialsEvent e) {
+        WebAuthenticationDetails auth = (WebAuthenticationDetails)
+                e.getAuthentication().getDetails();
+
+        loginAttemptService.loginFailed(auth.getRemoteAddress());
+    }
+}
