d0d1ea608f9643ba04fbd917824d2a5b3cc6eca7
Kudzai Huni <sean2kay@gmail.com>
2018-07-15 12:55:57 +0200

    Validations Working bug still buggy.

--- src/main/java/io/home/pi/web/util/GenericResponse.java
+++ src/main/java/io/home/pi/web/util/GenericResponse.java
@@ -3,6 +3,7 @@ package io.home.pi.web.util;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 
@@ -16,6 +17,7 @@ import java.util.stream.Collectors;
  * DATE      : 30-June-2018
  * TIME      : 13:14
  */
+@Slf4j
 @Getter
 @Setter
 public class GenericResponse {
@@ -24,12 +26,14 @@ public class GenericResponse {
     @Getter(AccessLevel.NONE)
     @Setter(AccessLevel.NONE)
     private Boolean successful;
+//    private MessageSource messageSource;
 
     public GenericResponse(final String message, final Boolean successful) {
         super();
         this.message = message;
         this.successful = successful;
     }
+
 
     public GenericResponse(final String message, final String errorMsg, final Boolean successful) {
         super();
@@ -43,14 +47,21 @@ public class GenericResponse {
         this.errorMsg = errorMsg;
         String temp = allErrors.stream().map(e -> {
             if (e instanceof FieldError) {
+//                return "{\"field\":\"" + ((FieldError) e).getField() + "\",\"defaultMessage\":\"" + stripErrorCode(e.getDefaultMessage()) + "\"}";
                 return "{\"field\":\"" + ((FieldError) e).getField() + "\",\"defaultMessage\":\"" + e.getDefaultMessage() + "\"}";
             } else {
+//                return "{\"object\":\"" + e.getObjectName() + "\",\"defaultMessage\":\"" + stripErrorCode(e.getDefaultMessage()) + "\"}";
                 return "{\"object\":\"" + e.getObjectName() + "\",\"defaultMessage\":\"" + e.getDefaultMessage() + "\"}";
             }
         }).collect(Collectors.joining(","));
         this.message = "[" + temp + "]";
     }
 
+//    @Autowired
+//    @Qualifier("english")
+//    public void setMessageSource(MessageSource messageSource) {
+//        this.messageSource = messageSource;
+//    }
 
     public Boolean isSuccess() {
         return successful;
@@ -59,4 +70,19 @@ public class GenericResponse {
     public void setSuccess(Boolean successful) {
         this.successful = successful;
     }
+
+//    private String stripErrorCode(String errorMsg) {
+//        if (errorMsg == null) {
+//            return errorMsg;
+//        }
+//
+//        int firstBrace = errorMsg.indexOf("{");
+//        if (firstBrace == -1) {
+//            return errorMsg;
+//        }
+//
+//        final String messageSourceTxt = messageSource.getMessage(errorMsg.substring(firstBrace + 1, errorMsg.indexOf("}")), null, Locale.UK);
+//        log.info("Source-Txt: {}", messageSourceTxt);
+//        return messageSourceTxt;
+//    }
 }
--- src/main/resources/public/js/register.js
+++ src/main/resources/public/js/register.js
@@ -5,6 +5,7 @@
  * DATE    : Monday-July-2018
  */
 
+var map = {};
 /*<![CDATA[*/
 $(document).ready(
     function () {
@@ -23,5 +24,211 @@ $(document).ready(
     }
 );
 
-
 /*]]>*/
+
+function ajxUsrRegister(jsonObjStr) {
+    $.ajax({
+        type: 'POST',
+        url: '/register/user',
+        dataType: 'json',
+        contentType: 'application/json',
+        data: jsonObjStr,
+        cache: false,
+        timeout: 10000,
+        success: function (data) {
+            console.log("SUCCESS : ", data);
+            // if (data.isSuccess == true) {
+            clearRegistrationFormData();
+            console.log("Form-Data Cleared...");
+            $('#registrationModal').modal('hide');
+            showSuccessPopup("Email Confirmation", data.message);
+            // } else {
+            //     showErrPopup("Unexpected Response", "Unexpected response from the system.");
+            // }
+            $("#btnRegisterUser").prop("disabled", false);
+        },
+        error: function (error) {
+            if (error.status === 400) {
+                var popupErrorTitle = titleErrPopupMsg("Validation Error", error.responseJSON.errors.length);
+                var popupErrMsg = appendErrMessages(error);
+
+                showErrPopup(popupErrorTitle, popupErrMsg)
+            } else {
+                console.log("Error: \n" + error.responseJSON);
+                showErrPopup("System Failure", "Unexpected response from the system.");
+            }
+
+            // console.log("Error: ", error);
+            $("#btnRegisterUser").prop("disabled", false);
+        }
+    });
+}
+
+
+function ajaxCall(urlWithData) {
+
+    $.ajax({
+        type: 'GET',
+        url: urlWithData,
+        dataType: 'json',
+        contentType: 'application/json',
+        cache: false,
+        timeout: 10000,
+        success: function (data) {
+            var getResp = new Map();
+            $.each(data, function (key, val) {
+                console.log("Key: " + key + "\nVal: " + val);
+                getResp.set(key, val);
+                $('#map').hide().append("<li id='" + key + "'>" + val + "</li>");
+            });
+        },
+        error: function (error) {
+            showErrPopup("System Failure", "Unexpected response from the system.");
+            $("#btnRegisterUser").prop("disabled", false);
+        }
+    });
+
+
+}
+
+function findSourceMatch(source) {
+    var vResp = "";
+    // $('#map li').each(function () {
+    //     if ($(this).attr('id') === source) {
+    //         // $(this).attr('id');
+    //         return $(this).text();
+    //     } else {
+    //         console.log("MAP is Null/Empty...");
+    //     }
+    //     return "";
+    // });
+
+    for (var i = 0; i < $('#map li').length; i++) {
+        if ($('#map li').eq(i).attr('id') == source) {
+            vResp = $('#map li').eq(i).text();
+        }
+    }
+
+    return vResp;
+}
+
+
+function messageSourceSubstitute(codes) {
+    var codesUrl = "";
+    // var resp = new Map();
+    var firstIter = true;
+
+    $.each(codes, function (index, item) {
+        if (firstIter) {
+            codesUrl += "code=" + item;
+            firstIter = false;
+        } else {
+            codesUrl += "&code=" + item;
+        }
+    });
+
+    ajaxCall("http://localhost:8080/register/messageSourceList?" + codesUrl);
+
+
+    // $.get("http://localhost:8080/register/messageSourceList?" + codesUrl,
+    //     {async: false},
+    //     function (data, status, xhr) {
+    //         var getResp = new Map();
+    //         $.each(data, function (key, val) {
+    //             console.log("Key: " + key + "\nVal: " + val);
+    //             getResp.set(key, val);
+    //         });
+    //
+    //         resp = getResp;
+    //     }).done(function () {
+    //     alert(resp);
+    // });
+}
+
+
+function titleErrPopupMsg(errTitle, errorsLength) {
+    if (errorsLength > 1) {
+        return errTitle + "s";
+    }
+}
+
+function appendErrMessages(errorsObj) {
+
+    var errMsg = "";
+    $.each(errorsObj.responseJSON.errors, function (index, item) {
+        errMsg += item.defaultMessage + "<br/>";
+    });
+    var errMsg2 = errMsg.replace(/,/g, "<br/>");
+    var errMsg3 = errMsg2.replace(/{/g, "");
+    errMsg3 = errMsg3.replace(/}/g, "");
+    console.log("Msg: \n", errMsg3);
+
+    var keyMappings = extractKeyErrorMsgs(errMsg2, "{", "}");
+    console.log("Extract: \n", keyMappings);
+    var errorMsg = replaceErrorCodesWithMsgs(errMsg3, keyMappings);
+    console.log("Extract Replaced: \n", errorMsg);
+    return errorMsg;
+}
+
+function extractKeyErrorMsgs(str, start, end) {
+
+    var getFromBetween = {
+        results: [],
+        string: "",
+        getFromBetween: function (sub1, sub2) {
+            if (this.string.indexOf(sub1) < 0 || this.string.indexOf(sub2) < 0) return false;
+            var SP = this.string.indexOf(sub1) + sub1.length;
+            var string1 = this.string.substr(0, SP);
+            var string2 = this.string.substr(SP);
+            var TP = string1.length + string2.indexOf(sub2);
+            return this.string.substring(SP, TP);
+        },
+        removeFromBetween: function (sub1, sub2) {
+            if (this.string.indexOf(sub1) < 0 || this.string.indexOf(sub2) < 0) return false;
+            var removal = sub1 + this.getFromBetween(sub1, sub2) + sub2;
+            this.string = this.string.replace(removal, "");
+        },
+        getAllResults: function (sub1, sub2) {
+            // first check to see if we do have both substrings
+            if (this.string.indexOf(sub1) < 0 || this.string.indexOf(sub2) < 0) return;
+
+            // find one result
+            var result = this.getFromBetween(sub1, sub2);
+            // push it to the results array
+            this.results.push(result);
+            // remove the most recently found one from the string
+            this.removeFromBetween(sub1, sub2);
+
+            // if there's more substrings
+            if (this.string.indexOf(sub1) > -1 && this.string.indexOf(sub2) > -1) {
+                this.getAllResults(sub1, sub2);
+            }
+            else return;
+        },
+        get: function (string, sub1, sub2) {
+            this.results = [];
+            this.string = string;
+            this.getAllResults(sub1, sub2);
+            return this.results;
+        }
+    };
+
+    var errorKeys = getFromBetween.get(str, start, end);
+
+    return errorKeys;
+}
+
+
+function replaceErrorCodesWithMsgs(strMsg, arrayCodes) {
+    var newMsgs = strMsg;
+
+    messageSourceSubstitute(arrayCodes);
+
+    $.each(arrayCodes, function (index, item) {
+        var re = new RegExp(item.toString(), "ig");
+        $("#msgSourceId").text(item.toString());
+        newMsgs = newMsgs.replace(re, findSourceMatch(item.toString())); //FixMe replace match with messageSource.
+    });
+
+    return newMsgs;
+}
